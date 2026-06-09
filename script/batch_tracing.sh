f8b5f6eba45dc79fb63ceb41276aff536f97be97
fouziray <faouzirayan@gmail.com>
2026-06-09 17:18:59 +0200

    clone spec book in right plqce

--- script/batch_tracing.sh
+++ script/batch_tracing.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-git clone --quiet --depth=1 https://github.com/SquareBracketAssociates/BuildingApplicationWithSpec2.git Spec2Book
-
 #benches=("HoneyGinger" "DataFrame" "Bloc" "Microdown")
 benches=("Microdown")
 
 for bench in "${benches[@]}"; do
 	mkdir temp
 	cd temp
+
+	if [ "$bench" = "Microdown" ]; then
+		git clone --quiet --depth=1 https://github.com/SquareBracketAssociates/BuildingApplicationWithSpec2.git Spec2Book
+	fi
+
 	wget -O - get.pharo.org/120+vm | bash
 	./pharo Pharo.image metacello install github://fouziray/staticCallPharo:traceWithTypePattern BaselineOfStaticCallExp  --groups=mintracing`echo ${bench} | tr "[:upper:]" "[:lower:]"`
     ./pharo Pharo.image eval "PatternCallSites tracePatternsOf${bench}"
