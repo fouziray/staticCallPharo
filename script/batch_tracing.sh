fdb46ea9cd86fa3d3136c76eeaa09d26d3bccd38
fouziray <faouzirayan@gmail.com>
2026-06-09 16:38:05 +0200

    Fix tr

--- script/batch_tracing.sh
+++ script/batch_tracing.sh
@@ -9,7 +9,7 @@ for bench in "${benches[@]}"; do
 	mkdir temp
 	cd temp
 	wget -O - get.pharo.org/120+vm | bash
-	./pharo Pharo.image metacello install github://fouziray/staticCallPharo:traceWithTypePattern BaselineOfStaticCallExp  --groups=mintracing`tr ${bench}`
+	./pharo Pharo.image metacello install github://fouziray/staticCallPharo:traceWithTypePattern BaselineOfStaticCallExp  --groups=mintracing`echo ${bench} | tr "[:upper:]" "[:lower:]"`
     ./pharo Pharo.image eval "PatternCallSites tracePatternsOf${bench}"
 	cp *.ston *.csv ..
 	cd ..
