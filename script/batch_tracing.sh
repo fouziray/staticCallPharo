7d13a2ea36796046b04f83f3044c0c9fadf1ff1b
fouziray <faouzirayan@gmail.com>
2026-06-09 16:33:58 +0200

    The best bash script

--- script/batch_tracing.sh
+++ script/batch_tracing.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-wget -O - get.pharo.org/120+vm | bash
-./pharo Pharo.image metacello install github://fouziray/staticCallPharo:traceWithTypePattern BaselineOfStaticCallExp  --groups=mintracingveritas
+git clone --quiet --depth=1 https://github.com/SquareBracketAssociates/BuildingApplicationWithSpec2.git Spec2Book
 
-
-
-benches=("HoneyGinger" "DataFrame")
+#benches=("HoneyGinger" "DataFrame" "Bloc" "Microdown")
+benches=("Microdown")
 
 for bench in "${benches[@]}"; do
-    ./pharo Pharo.image eval “PatternCallSites tracePatternsOf${bench}"
+	mkdir temp
+	cd temp
+	wget -O - get.pharo.org/120+vm | bash
+	./pharo Pharo.image metacello install github://fouziray/staticCallPharo:traceWithTypePattern BaselineOfStaticCallExp  --groups=mintracing`tr ${bench}`
+    ./pharo Pharo.image eval "PatternCallSites tracePatternsOf${bench}"
+	cp *.ston *.csv ..
+	cd ..
+	rm -rf temp
 done
