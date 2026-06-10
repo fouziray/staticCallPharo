04ceeb45c5fcfb5e1ecd508775f9e88ad11923ea
fouziray <faouzirayan@gmail.com>
2026-06-10 17:25:40 +0200

    bash script to run dataframe trace and generate type csv file

--- script/batch_tracing.sh
+++ script/batch_tracing.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -x;
 
 #benches=("HoneyGinger" "DataFrame" "Bloc" "Microdown")
-benches=("Microdown")
+#veritas standsfor Dataframe baseline
+
+benches=("DataFrame")
 
 for bench in "${benches[@]}"; do
 	mkdir temp
@@ -11,9 +14,27 @@ for bench in "${benches[@]}"; do
 		git clone --quiet --depth=1 https://github.com/SquareBracketAssociates/BuildingApplicationWithSpec2.git Spec2Book
 	fi
 
+	#Load a Pharo12 image and patch it to allow loading of packages such as Moose
 	wget -O - get.pharo.org/120+vm | bash
+	./pharo Pharo.image eval --save "IceTipRepositoriesModel class compile: 'addTag: t priority: p'"
+
 	./pharo Pharo.image metacello install github://fouziray/staticCallPharo:traceWithTypePattern BaselineOfStaticCallExp  --groups=mintracing`echo ${bench} | tr "[:upper:]" "[:lower:]"`
-    ./pharo Pharo.image eval "PatternCallSites tracePatternsOf${bench}"
+	
+	if [ "$bench" = "Moose" ]; then
+		cp pharo-local/iceberg/fouziray/PharoVeritasBenchSuite/files/sbscl.json .
+	fi
+	
+	if [ "$bench" = "DataFrame" ]; then
+		cp pharo-local/iceberg/fouziray/PharoVeritasBenchSuite/files/tiny_dataset.csv .
+	fi
+	
+	
+	if [ "$bench" = "Microdown" ]; then
+			git clone --quiet --depth=1 https://github.com/SquareBracketAssociates/BuildingApplicationWithSpec2.git Spec2Book
+	fi
+	
+	./pharo Pharo.image eval "PatternCallSites tracePatternsOf${bench}"
+
 	cp *.ston *.csv ..
 	cd ..
 	rm -rf temp
